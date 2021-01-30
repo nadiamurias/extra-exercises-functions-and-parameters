b96bebcad23c749ac3889c3cad8a2d5b3dda9d95
Nadia Murias <nadiamurias@gmail.com>
2021-01-30 16:22:36 +0100

    Add exercise fase 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 "use strict";
-
+// ex fase 1
 function ten(p) {
   return p.repeat(10);
 }
 console.log(ten("patata"));
+
+//ex fase 2
+
+function ten(word) {
+  return word;
+}
+console.log(ten("patata"));
+console.log(ten("Aguacate"));
+console.log(ten("Pizza"));
